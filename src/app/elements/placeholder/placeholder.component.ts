b36ef9f205a07413a0e5ce8de034da295c41bdeb
tn-teamrg <85945974+tn-teamrg@users.noreply.github.com>
2022-06-09 09:03:35 -0400

    add customized components

---
+++ src/app/elements/placeholder/placeholder.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-placeholder',
+  templateUrl: './placeholder.component.html',
+  styleUrls: ['./placeholder.component.css']
+})
+export class PlaceholderComponent implements OnInit {
+  @Input() header = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
