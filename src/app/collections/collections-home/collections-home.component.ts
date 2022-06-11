cc17ad0591038fb64c9b69c9d97611227b2f8347
tn-teamrg <85945974+tn-teamrg@users.noreply.github.com>
2022-06-11 15:31:54 -0400

    add table data/headers inputs; add table skeleton; add mock data to table; add propertiess to table component; add custom boolean attribute to table data

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'Gosling', age: 67, job: 'Dr. Java', employed: true },
+    { name: 'Johnson', age: 27, job: 'Spring Framework Creator', employed: false },
+    { name: 'Leroy', age: 22, job: 'Pen Tester', employed: true },
+    { name: 'L', age: 33, job: 'UI Architect', employed: true },
+  ];
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' },
+    { key: 'employed', label: 'Has a job' },
+  ];
 
   constructor() { }
 
