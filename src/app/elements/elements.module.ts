a1f8bc1afb6fd6b1effc18aaf39487007f1c1e4d
tn-teamrg <85945974+tn-teamrg@users.noreply.github.com>
2022-06-11 13:49:00 -0400

    add parameterized divider component

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -5,17 +5,19 @@ import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { TimesDirective } from './times.directive';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
     ElementsHomeComponent,
     PlaceholderComponent,
-    TimesDirective
+    TimesDirective,
   ],
   imports: [
     CommonModule,
-    ElementsRoutingModule
+    ElementsRoutingModule,
+    SharedModule
   ]
 })
 export class ElementsModule { }
---
+++ src/app/shared/divider/divider.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-divider',
+  templateUrl: './divider.component.html',
+  styleUrls: ['./divider.component.css']
+})
+export class DividerComponent implements OnInit {
+  @Input() title="Default Title";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
