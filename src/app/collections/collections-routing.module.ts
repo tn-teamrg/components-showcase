eddfc71993f94fa62c1fc5654cb1d7f01a1fc0bc
tn-teamrg <85945974+tn-teamrg@users.noreply.github.com>
2022-06-11 16:03:22 -0400

    add child navigation routes; add default route (biography); add components to collections module; add sub-components (partners/biography/companies)

--- src/app/collections/collections-routing.module.ts
+++ src/app/collections/collections-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
+import { BiographyComponent } from './biography/biography.component';
 
 const routes: Routes = [
-  { path: '', component: CollectionsHomeComponent }
+  { path: '', component: CollectionsHomeComponent,
+    children: [
+      { path: '', component: BiographyComponent },
+      { path: 'companies', component: CompaniesComponent },
+      { path: 'partners', component: PartnersComponent }
+    ]
+}
 ];
 
 @NgModule({
