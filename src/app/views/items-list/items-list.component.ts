7ab0a670333efd0746f15f94db431b68f7bf33ee
tn-teamrg <85945974+tn-teamrg@users.noreply.github.com>
2022-06-11 18:31:43 -0400

    add divider css ; add items list component to views module; add items list html; add items component to views home landing page; add mock data for items component; add images

---
+++ src/app/views/items-list/items-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-items-list',
+  templateUrl: './items-list.component.html',
+  styleUrls: ['./items-list.component.css']
+})
+
+export class ItemsListComponent implements OnInit {
+  @Input() items: any = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-views-home',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./views-home.component.css']
 })
 export class ViewsHomeComponent implements OnInit {
+
   stats = [
     { value: 22, label: '# of Users' },
     { value: 900, label: 'Revenue' },
     { value: 50, label: 'Reviews' }
+  ];
+
+  items = [
+    { image: '/assets/images/couch.jpeg', title: 'Couch', description: 'couch description goes here'},
+    { image: '/assets/images/dresser.jpeg', title: 'Dresser', description: 'dresser description goes here'}
   ];
 
   constructor() { }
