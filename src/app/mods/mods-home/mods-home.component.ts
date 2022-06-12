d7fb922c6d672439b77a549ec68cedd4124516c3
tn-teamrg <85945974+tn-teamrg@users.noreply.github.com>
2022-06-12 00:28:05 -0400

    add accordion component

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen: boolean = false;
+  items = [
+    { title: 'title1', content: 'Laborum labore esse qui excepteur non.'},
+    { title: 'title2', content: 'Do id do laborum proident laboris.'},
+    { title: 'title3', content: 'Lorem aute voluptate quis ipsum sit pariatur mollit pariatur quis deserunt.'},
+    { title: 'title4', content: 'Laborum esse labore duis velit mollit ipsum nostrud in adipisicing.'},
+    { title: 'title5', content: 'Ad ut duis officia irure culpa.'},
+
+  ];
 
   constructor() { }
 
